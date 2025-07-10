763d6f1af2eb87c4cfd0d3b15f6039c90003fca7
Jayesh Saini <jayeshsaini@Jayeshs-MacBook-Air.local>
2025-07-09 23:47:10 -0400

    android live update notification working

--- example/App.tsx
+++ example/App.tsx
@@ -1,43 +1,93 @@
-import { useEvent } from 'expo';
-import ExpoLiveUpdatesActivities from 'expo-live-updates-activities';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { useEvent } from "expo";
+import ExpoLiveUpdatesActivities from "expo-live-updates-activities";
+import { ScrollView, Text, View, Platform, Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  const onChangePayload = useEvent(ExpoLiveUpdatesActivities, 'onChange');
+  // const onChangePayload = useEvent(ExpoLiveUpdatesActivities, 'onChange');
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        <Text style={styles.header}>Module API Example</Text>
-       <View style={styles.container}>
-      <Text
-        onPress={() => {
-          ExpoLiveUpdatesActivities.startActivity("Hello", "🚀");
-        }}
-      >
-        Start Activity
-      </Text>
-      <Text
-        onPress={() => {
-          const emojis = ["🚀", "🥓", "🔥", "⚡️"];
-          ExpoLiveUpdatesActivities.updateActivity(
-            emojis[Math.floor(Math.random() * emojis.length)]
-          );
-        }}
-      >
-        Update Activity
-      </Text>
-      <Text
-        onPress={() => {
-          ExpoLiveUpdatesActivities.endActivity();
-        }}
-      >
-        End Activity
-      </Text>
-    </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  if (Platform.OS === "ios") {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Text style={styles.header}>Module API Example</Text>
+          <View style={styles.container}>
+            <Text
+              onPress={() => {
+                ExpoLiveUpdatesActivities.startActivity("Hello", "🚀");
+              }}
+            >
+              Start Activity
+            </Text>
+            <Text
+              onPress={() => {
+                const emojis = ["🚀", "🥓", "🔥", "⚡️"];
+                ExpoLiveUpdatesActivities.updateActivity(
+                  emojis[Math.floor(Math.random() * emojis.length)]
+                );
+              }}
+            >
+              Update Activity
+            </Text>
+            <Text
+              onPress={() => {
+                ExpoLiveUpdatesActivities.endActivity();
+              }}
+            >
+              End Activity
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Text style={styles.header}>Module API Example</Text>
+          <View style={styles.container}>
+            <Button
+              title="Start Live Update Notification"
+              onPress={() => {
+                ExpoLiveUpdatesActivities.showLiveUpdateNotification();
+              }}
+            />
+            {/* <Text
+              onPress={() => {
+                // Show/update progress notification (Android)
+                ExpoLiveUpdatesActivities.showLiveUpdate(
+                  50,
+                  "Order in progress",
+                  "Your order is being prepared"
+                );
+              }}
+            >
+              Show Live Update (Android)
+            </Text>
+            <Text
+              onPress={() => {
+                // Complete notification (Android)
+                ExpoLiveUpdatesActivities.completeLiveUpdate(
+                  "Order complete",
+                  "Thank you!"
+                );
+              }}
+            >
+              Complete Live Update (Android)
+            </Text>
+            <Text
+              onPress={() => {
+                // Cancel notification (Android)
+                ExpoLiveUpdatesActivities.cancelLiveUpdate();
+              }}
+            >
+              Cancel Live Update (Android)
+            </Text> */}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 function Group(props: { name: string; children: React.ReactNode }) {
@@ -60,13 +110,13 @@ const styles = {
   },
   group: {
     margin: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
   },
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
   },
   view: {
     flex: 1,
